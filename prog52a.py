f9925072b3513644145b79170cb782106281a82a
Farrell146 <153585781+Farrell146@users.noreply.github.com>
2023-12-12 06:14:03 +0000

    12.12

---
+++ prog52a.py
@@ -0,0 +1,10 @@
+#int for whole
+#float for decimals
+#str for strings
+
+length = int(input("enter length:"))
+width = int(input("enter width"))
+area = length*width
+perem = (length*2) + (width*2)
+print("Area is:" + str(area))
+print("Perimeter is:",perem)
