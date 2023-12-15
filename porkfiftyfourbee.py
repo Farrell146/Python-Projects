b03831b5a517d16ca263b534829593ba6be3e547
Farrell146 <153585781+Farrell146@users.noreply.github.com>
2023-12-15 23:19:09 +0000

    meganumbas

---
+++ porkfiftyfourbee.py
@@ -0,0 +1,26 @@
+#int for whole
+#float for decimals
+#str for strings
+
+numbaone = int(input("enta numba one: "))
+numbatwo = int(input("enta numba two: "))
+numbathree = int(input("enta numba three"))
+numbafour = int(input("enta numba four: "))
+
+meganumba = numbaone + numbatwo + numbathree + numbafour
+
+wackassnumba = meganumba / 4
+
+print("Fuck you its: "+str(meganumba))
+print("Averys cock is: "+str(wackassnumba)+" this big in meters.")
+
+
+#/home/codespace/.python/current/bin/python3 /workspaces/Python-Projects/porkfiftyfourbee.py
+#@Farrell146 ➜ /workspaces/Python-Projects (main) $ /home/codespace/.python/current/bin/python3 /workspaces/Python-Projects/porkfiftyfourbee.py
+#enta numba one: 1
+#enta numba two: 2
+#enta numba three3
+#enta numba four: 4
+#Fuck you its: 10
+#Averys cock is: 2.5 this big in meters.
+#@Farrell146 ➜ /workspaces/Python-Projects (main) $ 
