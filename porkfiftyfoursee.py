1b845492ba6907ebc2bc2f6f379db58799668098
Farrell146 <153585781+Farrell146@users.noreply.github.com>
2023-12-15 23:29:19 +0000

    unga

---
+++ porkfiftyfoursee.py
@@ -0,0 +1,16 @@
+#int for whole
+#float for decimals
+#str for strings
+
+pie = 3.14159
+radius = int(input("Enter radius: "))
+
+sircumfence = 2 * pie * radius
+area = pie * radius**2
+
+print("The thing is equal to:", round(sircumfence, 3))
+print("Area: ",round(area, 3))
+
+#Enter radius: 3
+#The thing is equal to: 18.85
+#Area:  28.274
