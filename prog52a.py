8d0f1be81feaa9cb08e6a3d14d805a6d8e42733f
Josh Farrell <farrell.j339@gmail.com>
2023-12-15 20:08:10 -0600

    shit butt

--- prog52a.py
+++ prog52a.py
@@ -2,9 +2,10 @@
 #float for decimals
 #str for strings
 
-length = int(input("enter length:"))
-width = int(input("enter width"))
+length = int(input("enter length: "))
+width = int(input("enter width: "))
 area = length*width
 perem = (length*2) + (width*2)
-print("Area is:" + str(area))
-print("Perimeter is:",perem)
+print("Area is: " + str(area))
+
+print("Perimeter is: ",perem)
