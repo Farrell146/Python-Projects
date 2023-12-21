5f450d3496e8c5dade08c417576f344514ae2182
Josh Farrell <farrell.j339@gmail.com>
2023-12-20 19:31:53 -0600

    12.20

---
+++ lp4-3.py
@@ -0,0 +1,23 @@
+eggs = int(input("Number of eggs: "))
+
+# "//" divides then rounds down to nearest int
+doz = eggs//12
+
+# "%" is modulus - the remainder of long division
+rem = eggs%12
+
+calc = 0
+
+if 0 < doz <= 4:
+    calc = .50
+elif doz > 4 and doz <= 6:
+    calc = .45
+elif doz > 6 and doz <= 11:
+    calc = .40
+elif doz > 11:
+    calc = .35
+
+print("The bill is equal to: $"+str((doz*calc)+(rem*(calc*(1/12)))))
+
+"""Number of eggs: 18
+The bill is equal to: $0.75"""
