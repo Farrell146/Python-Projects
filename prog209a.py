47cdb886fcf264676f80846896e48ddbd673306f
Josh Farrell <farrell.j339@gmail.com>
2023-12-26 17:19:54 -0600

    12.26

---
+++ prog209a.py
@@ -0,0 +1,24 @@
+try: 
+    a = 0
+    b = 0
+    with open ("data/prog215a.dat") as f:
+        lines = f.readlines()
+        for line in lines:
+            c = int(line)
+            if c<500:
+                a+=1
+            else:
+                b+=1
+
+    print("Less than 500:",a)
+    print("Is or Greater than 500:",b)
+    print("Total numbers:",a+b)
+
+except Exception as e:
+    print(e)
+
+"""
+Less than 500: 192
+Is or Greater than 500: 208
+Total numbers: 400
+"""
