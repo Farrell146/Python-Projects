deacbdca555498085f284dfa5548f279e564eef6
Josh Farrell <farrell.j339@gmail.com>
2023-12-26 17:22:47 -0600

    12.26

--- prog209a.py
+++ prog209a.py
@@ -22,3 +22,9 @@ Less than 500: 192
 Is or Greater than 500: 208
 Total numbers: 400
 """
+
+"""
+with open ("data/prog215a.dat") as f: - opens up a data file
+        lines = f.readlines() - lines is declared as a list
+        for line in lines:  - will automatically add 1 to the line variable
+"""
